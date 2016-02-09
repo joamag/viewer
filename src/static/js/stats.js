baf270807576062a1c41f615b2ac0225f7e2e89b
joamag <joamag@gmail.com>
2016-02-09 20:56:57 +0000

    new license values

--- src/static/js/stats.js
+++ src/static/js/stats.js
@@ -4,24 +4,24 @@
 // This file is part of Hive Three Extensions.
 //
 // Hive Three Extensions is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// it under the terms of the Apache License as published by the Apache
+// Foundation, either version 2.0 of the License, or (at your option) any
+// later version.
 //
 // Hive Three Extensions is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// Apache License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Hive Three Extensions. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the Apache License along with
+// Hive Three Extensions. If not, see <http://www.apache.org/licenses/>.
 
 // __author__    = João Magalhães <joamag@hive.pt>
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision$
 // __date__      = $LastChangedDate$
-// __copyright__ = Copyright (c) 2010-2012 Hive Solutions Lda.
-// __license__   = GNU General Public License (GPL), Version 3
+// __copyright__ = Copyright (c) 2008-2016 Hive Solutions Lda.
+// __license__   = Apache License, Version 2.0
 // __credits__   = Ricardo Miguel <mrdoob@gmail.com>
 
 var THREEx = THREEx || {};
